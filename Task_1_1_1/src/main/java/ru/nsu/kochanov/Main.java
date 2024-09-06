2b64ceac579e29af02c127b734ee6ddcf248e607
Nikita Kochanov <n.kochanov@g.nsu.ru>
2024-09-06 15:48:42 +0700

    sample1

--- Task_1_1_1/src/main/java/ru/nsu/kochanov/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/kochanov/Main.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     // Метод сортировки
-    public void sort(int arr[]) {
+    public void sort(int[] arr) {
         int n = arr.length;
 
         // Построение кучи (Heapify)
-        for (int i = n / 2 - 1; i >= 0; i--)
+        for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
+        }
 
         // Извлечение элементов из кучи
         for (int i = n - 1; i >= 0; i--) {
@@ -25,18 +26,20 @@ public class Main {
     }
 
     // Метод для приведения поддерева с корнем в узле i к куче
-    void heapify(int arr[], int n, int i) {
-        int largest = i; // Инициализируем наибольший элемент как корень
-        int l = 2 * i + 1; // левый = 2*i + 1
-        int r = 2 * i + 2; // правый = 2*i + 2
+    void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
         // Если левый дочерний элемент больше корня
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
 
         // Если правый дочерний элемент больше, чем наибольший элемент на данный момент
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
 
         // Если наибольший элемент не корень
         if (largest != i) {
@@ -50,22 +53,25 @@ public class Main {
     }
 
     // Метод для вывода массива
-    static void printArray(int arr[]) {
+    static void printArray(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
+        }
         System.out.println();
     }
 
     // Основная программа
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Ввод размера массива
+        System.out.print("Введите размер массива: ");
         int n = scanner.nextInt();
-        int arr[] = new int[n];
+        int[] arr = new int[n];
 
         // Ввод элементов массива
+        System.out.println("Введите элементы массива:");
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
@@ -75,8 +81,9 @@ public class Main {
         ob.sort(arr);
 
         // Вывод отсортированного массива
+        System.out.println("Отсортированный массив:");
         printArray(arr);
 
-        scanner.close(); // Закрытие сканера
+        scanner.close();  // Закрытие сканера
     }
 }
