2fcab8fcf07c5dce70a5551c43d295945a6decb3
Nikita Kochanov <n.kochanov@g.nsu.ru>
2024-09-20 12:46:28 +0700

    test file added

--- Task_1_1_2/script.sh
+++ Task_1_1_2/script.sh
@@ -1,2 +1,3 @@
 javac -d out src/main/java/ru/nsu/kochanov/*.java
-jar cvfm black.jar ru.nsu.kochanov.Main -C out .
+jar cf myprogram.jar -C out .
+java -cp myprogram.jar ru.nsu.kochanov.Main
---
+++ Task_1_1_2/src/test/java/ru/nsu/kochanov/MainTest.java
@@ -0,0 +1,6 @@
+package ru.nsu.kochanov;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MainTest {
+  
+}
