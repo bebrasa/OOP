0b4d2ce0632f3b538546401d44ec598af66eba80
bebrasa <bebrasa@users.noreply.github.com>
2024-09-20 05:47:11 +0000

    Deploying to gh-pages from @ bebrasa/OOP@58c50ef5fa82ae8b4991d4b4d89e8e2aaf19a3ba 🚀

---
+++ Task_1_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kochanov","l":"Card"},{"p":"ru.nsu.kochanov","l":"Deck"},{"p":"ru.nsu.kochanov","l":"GameLogic"},{"p":"ru.nsu.kochanov","l":"Main"},{"p":"ru.nsu.kochanov","l":"Player"}]
