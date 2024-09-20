e201ff1fc006faaa082368f9ef7500607af8ada6
Nikita Kochanov <n.kochanov@g.nsu.ru>
2024-09-20 13:35:24 +0700

    test updates1

--- Task_1_1_2/src/main/java/ru/nsu/kochanov/Deck.java
+++ Task_1_1_2/src/main/java/ru/nsu/kochanov/Deck.java
@@ -6,40 +6,38 @@ import java.util.Random;
 
 public class Deck {
 
-    private List<Card> cards;
-    //Random rand = new Random();
-    //private int n = rand.nextInt(1,9);
+  private List<Card> cards;
 
-    public Deck() {
+  public Deck() {
 
-        cards = new ArrayList<Card>();
+    cards = new ArrayList<Card>();
 
-        String ranks[] = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"};
-        String suits[] = new String[]{"червы", "трефы", "пики", "бубны"};
+    String ranks[] = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"};
+    String suits[] = new String[]{"червы", "трефы", "пики", "бубны"};
 
-        for (String suit : suits) {
-            for (String rank : ranks) {
-                cards.add(new Card(rank, suit));
-            }
-        }
+    for (String suit : suits) {
+      for (String rank : ranks) {
+        cards.add(new Card(rank, suit));
+      }
     }
+  }
 
-    public void shuffle() {
-        Random rand = new Random();
-        for (int i = cards.size() - 1; i > 0; i--) {
-            int j = rand.nextInt(i + 1);
-            Card temp = cards.get(i);
-            cards.set(i, cards.get(j));
-            cards.set(j, temp);
-        }
+  public void shuffle() {
+    Random rand = new Random();
+    for (int i = cards.size() - 1; i > 0; i--) {
+      int j = rand.nextInt(i + 1);
+      Card temp = cards.get(i);
+      cards.set(i, cards.get(j));
+      cards.set(j, temp);
     }
+  }
 
-    public Card takeCard() {
-        if (cards.size() > 0) {
-            return cards.remove(cards.size() - 1);
-        }
-        return null;
+  public Card takeCard() {
+    if (cards.size() > 0) {
+      return cards.remove(cards.size() - 1);
     }
+    return null;
+  }
 
 
 }
--- Task_1_1_2/src/main/java/ru/nsu/kochanov/Player.java
+++ Task_1_1_2/src/main/java/ru/nsu/kochanov/Player.java
@@ -3,110 +3,112 @@ package ru.nsu.kochanov;
 import java.util.ArrayList;
 
 public class Player {
-    Deck deck = new Deck();
-    public ArrayList<Card> playerCards;
 
-    public Player(Deck deck) {
-        this.playerCards = new ArrayList<>();
-        this.deck.shuffle();
+  Deck deck = new Deck();
+  public ArrayList<Card> playerCards;
+
+  public Player(Deck deck) {
+    this.playerCards = new ArrayList<>();
+    this.deck.shuffle();
+  }
+
+  public void getPlayerCards() {
+    if (playerCards.isEmpty()) {
+      playerCards.add(deck.takeCard());
+      playerCards.add(deck.takeCard());
+    } else {
+      playerCards.add(deck.takeCard());
+    }
+  }
+
+  public int getPlayerCount() {
+    return playerCards.size();
+  }
+
+  public String ShowPlayerCards() {
+    StringBuilder cards = new StringBuilder(); // Создаем StringBuilder для эффективного формирования строки
+    for (Card card : playerCards) {
+      cards.append(card.getSuitRank()).append(", "); // Добавляем каждую карту и разделитель
     }
 
-    public void getPlayerCards() {
-        if (playerCards.isEmpty()) {
-            playerCards.add(deck.takeCard());
-            playerCards.add(deck.takeCard());
-        } else {
-            playerCards.add(deck.takeCard());
-        }
+    // Удаляем последний разделитель, если коллекция не пустая
+    if (cards.length() > 0) {
+      cards.setLength(cards.length() - 2); // Удаляем последние ', '
     }
 
-    public int getPlayerCount() {
-        return playerCards.size();
+    return cards.toString(); // Возвращаем сформированную строку
+  }
+
+  public String ShowLastCard() {
+    return playerCards.get(getPlayerCount() - 1).getSuitRank();
+  }
+
+  public int calcScore(ArrayList<Card> cards) {
+    int score = 0;
+    int acesCount = 0;
+
+    for (Card card : cards) {
+      String rank = card.getRank();
+      switch (rank) {
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+          score += Integer.parseInt(rank);
+          break;
+        case "10":
+        case "J":
+        case "Q":
+        case "K":
+          score += 10;
+          break;
+        case "A":
+          acesCount++;
+          break;
+      }
     }
-
-    public String ShowPlayerCards() {
-        StringBuilder cards = new StringBuilder(); // Создаем StringBuilder для эффективного формирования строки
-        for (Card card : playerCards) {
-            cards.append(card.getSuitRank()).append(", "); // Добавляем каждую карту и разделитель
-        }
-
-        // Удаляем последний разделитель, если коллекция не пустая
-        if (cards.length() > 0) {
-            cards.setLength(cards.length() - 2); // Удаляем последние ', '
-        }
-
-        return cards.toString(); // Возвращаем сформированную строку
+    for (int i = 0; i < acesCount; i++) {
+      if (score + 11 <= 21) {
+        score += 11;
+      } else {
+        score += 1;
+      }
     }
+    return score;
+  }
 
-    public String ShowLastCard() {
-        return playerCards.get(getPlayerCount() - 1).getSuitRank();
-    }
-
-    public int calcScore(ArrayList<Card> cards) {
-        int score = 0;
-        int acesCount = 0;
-
-        for (Card card : cards) {
-            String rank = card.getRank();
-            switch (rank) {
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                    score += Integer.parseInt(rank);
-                    break;
-                case "10":
-                case "J":
-                case "Q":
-                case "K":
-                    score += 10;
-                    break;
-                case "A":
-                    acesCount++;
-                    break;
-            }
-        }
-        for (int i = 0; i < acesCount; i++) {
-            if (score + 11 <= 21) {
-                score += 11;
-            } else {
-                score += 1;
-            }
-        }
-        return score;
-    }
-
-    public int playerScore() {
-        return calcScore(playerCards);
-    }
+  public int playerScore() {
+    return calcScore(playerCards);
+  }
 }
 
 class Dealer extends Player {
-    private ArrayList<Card> dealerCards;
 
-    public Dealer(Deck deck) {
-        super(deck);
-    }
+  private ArrayList<Card> dealerCards;
 
-    @Override
-    public String ShowPlayerCards() {
-        if (getPlayerCount() > 1) {
-            StringBuilder cards = new StringBuilder();
-            for (int i = 0; i < getPlayerCount() - 1; i++) {
-                cards.append(playerCards.get(i).getSuitRank()).append(", ");
-            }
-            cards.append("<скрытая карта>");
-            return cards.toString();
-        }
-        return super.ShowPlayerCards();
-    }
+  public Dealer(Deck deck) {
+    super(deck);
+  }
 
-    public String ShowAllCards() {
-        return super.ShowPlayerCards();
+  @Override
+  public String ShowPlayerCards() {
+    if (getPlayerCount() > 1) {
+      StringBuilder cards = new StringBuilder();
+      for (int i = 0; i < getPlayerCount() - 1; i++) {
+        cards.append(playerCards.get(i).getSuitRank()).append(", ");
+      }
+      cards.append("<скрытая карта>");
+      return cards.toString();
     }
+    return super.ShowPlayerCards();
+  }
+
+  public String ShowAllCards() {
+    return super.ShowPlayerCards();
+  }
 
 }
