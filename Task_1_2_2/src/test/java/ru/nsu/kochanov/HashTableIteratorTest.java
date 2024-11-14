51297e5174b192648da50372306f590307aba795
Nikita Kochanov <n.kochanov@g.nsu.ru>
2024-11-14 17:56:34 +0700

    aaaa

--- Task_1_2_2/src/test/java/ru/nsu/kochanov/HashTableIteratorTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/kochanov/HashTableIteratorTest.java
@@ -1,10 +1,9 @@
 package ru.nsu.kochanov;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 import java.util.ConcurrentModificationException;
 import java.util.NoSuchElementException;
