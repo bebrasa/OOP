deee550d7afecb2fdb72d9e12a69f20464b87423
bebrasa <bebrasa@users.noreply.github.com>
2025-05-28 12:31:11 +0000

    Deploying to gh-pages from @ bebrasa/OOP@e4568b69ea0a31ebc685f728c2d1b88000db6ec8 🚀

--- Task_1_2_2/type-search-index.js
+++ Task_1_2_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kochanov","l":"Entry"},{"p":"ru.nsu.kochanov","l":"HashTable"},{"p":"ru.nsu.kochanov","l":"HashTableIterator"},{"p":"ru.nsu.kochanov","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kochanov","l":"Entry"},{"p":"ru.nsu.kochanov","l":"HashTable"},{"p":"ru.nsu.kochanov","l":"HashTableIterator"},{"p":"ru.nsu.kochanov","l":"Main"}];updateSearchResults();
